8a7065cf2bd57b5cc10b076318f02b30d852fec5
sosthene <sosthenetimi94@gmail.com>
2023-08-08 21:06:39 +0300

    Adding the ship on the screen

--- Alien/alien_invasion.py
+++ Alien/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -12,6 +13,9 @@ def run_game():
                                       ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # Make a ship
+    ship = Ship(screen)
+
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
@@ -21,6 +25,7 @@ def run_game():
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
